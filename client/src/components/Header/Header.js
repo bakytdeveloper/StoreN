d0e68b1a283770d1e1045d7286dbf83fa047f08d
bakytdeveloper <bakytdeveloper@gmail.com>
2024-01-04 01:31:20 +0600

    Создал начальный код для шапки, без функционала

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,49 @@
+// src/components/Header/Header.js
+
+import React from 'react';
+import './Header.css';
+import { Link } from 'react-router-dom';
+
+import ins from "./instagram.png";
+import tel from "./telegram.png";
+import what from "./whatsapp.png";
+import tik from "./tik-tok.png";
+
+const Header = () => {
+    return (
+        <div className="header">
+            <Link to="/" className="brand">
+                <h1 className="brand"> Store <span className="sp">N</span></h1>
+            </Link>
+            <div className="contact">
+                <div className="phone">XXX-XX-XX-XX</div>
+                <div className="social-icons">
+                    <a className="icon"  href="https://www.tiktok.com/" >
+                        <img src={tik} alt="Instagram Icon" />
+                    </a>
+
+                    <a href="https://web.whatsapp.com/">
+                        <img className="icon" src={what} alt="Instagram Icon" />
+                    </a>
+                    <a   href="https://www.instagram.com/">
+                        <img className="icon" src={ins} alt="Instagram Icon" />
+                    </a>
+                    <a className="icon"  href="https://web.telegram.org/">
+                    {/*<a className="icon"  href="https://web.telegram.org/" target="_blank">*/}
+                        <img src={tel} alt="Instagram Icon" />
+                    </a>
+                </div>
+            </div>
+            <div className="auth-buttons">
+                <button>Login/Register</button>
+                <button>Profile</button>
+                <button>Cart</button>
+            </div>
+            <div className="search">
+                <input type="text" placeholder="Search" />
+            </div>
+        </div>
+    );
+};
+
+export default Header;
