d2e1376838af349b04822267a3b9801fbab308a2
bakytdeveloper <bakytdeveloper@gmail.com>
2024-01-30 21:18:43 +0600

    Создал модальное окна, с уточнением заказа

---
+++ client/src/components/AdminPanel/OrderDetailsModal.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import './OrderDetailsModal.css';
+
+const OrderDetailsModal = ({ order, onClose }) => {
+    return (
+        <div className="order-details-modal">
+            <div className="modal-content">
+                <button className="close-button" onClick={onClose}>
+                    &#10006;
+                </button>
+                <h2>Детали заказа</h2>
+                <div className="order-info">
+                    <div>
+                        <strong>Номер заказа:</strong> {order._id}
+                    </div>
+                    <div>
+                        <strong>Клиент:</strong> {order.user ? order.user.name : 'Гость'}
+                    </div>
+                    <div>
+                        <strong>Email:</strong> {order.user ? order.user.email : '-'}
+                    </div>
+                    <div>
+                        <strong>Адрес:</strong> {order.address ? order.address : '-'}
+                    </div>
+                    <div>
+                        <strong>Телефон:</strong> {order.phoneNumber ? order.phoneNumber : '-'}
+                    </div>
+                    <div>
+                        <strong>Способ оплаты:</strong> {order.user ? order.paymentMethod : '-'}
+                    </div>
+                    <div>
+                        <strong>Комментарии:</strong> {order.comments ? order.comments : '-'}
+                    </div>
+                    <div>
+                        <strong>Статус:</strong> {order.status}
+                    </div>
+                    <div>
+                        <strong>Дата изменения статуса:</strong>{' '}
+                        {order.statusHistory && order.statusHistory.length > 0
+                            ? new Date(order.statusHistory[order.statusHistory.length - 1].time).toLocaleString()
+                            : '-'}
+                    </div>
+                    <div>
+                        <strong>Дата создания заказа:</strong> {new Date(order.date).toLocaleString()}
+                    </div>
+                    <div>
+                        <strong>Товары:</strong>
+                        <ul>
+                            {order.products.map((item) => (
+                                <li key={item.product._id}>
+                                    <div>
+                                        <strong>Тип товара:</strong> {item.product.type}
+                                    </div>
+                                    <div>
+                                        <strong>Бренд:</strong> {item.product.brand}
+                                    </div>
+                                    <div>
+                                        <strong>Название товара:</strong> {item.product.name}
+                                    </div>
+                                    <div>
+                                        <strong>Описание:</strong> {item.product.description}
+                                    </div>
+                                    <div>
+                                        <strong>Количество:</strong> {item.quantity}
+                                    </div>
+                                    <div>
+                                        <strong>Цена за единицу:</strong> {item.product.price} KGS
+                                    </div>
+                                    <hr/>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div>
+
+                        <strong>Общая сумма заказа:</strong> {order.totalAmount.toFixed(2)} KGS
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default OrderDetailsModal;
--- client/src/components/AdminPanel/OrderList.js
+++ client/src/components/AdminPanel/OrderList.js
@@ -1,65 +1,3 @@
-// // src/components/Admin/OrderList.js
-// import React, { useState, useEffect } from 'react';
-//
-// const OrderList = () => {
-//     const [orders, setOrders] = useState([]);
-//
-//     useEffect(() => {
-//         // Функция для получения списка заказов с бэкенда
-//         const fetchOrders = async () => {
-//             try {
-//                 const response = await fetch('http://localhost:5500/api/orders/orders');
-//                 const data = await response.json();
-//                 setOrders(data);
-//             } catch (error) {
-//                 console.error('Fetch error:', error);
-//             }
-//         };
-//
-//         // Вызываем функцию для получения списка заказов
-//         fetchOrders();
-//     }, []);
-//
-//     return (
-//         <div>
-//             <h2>Список заказов</h2>
-//             <table>
-//                 <thead>
-//                 <tr>
-//                     <th>ID</th>
-//                     <th>Пользователь</th>
-//                     <th>Продукты</th>
-//                     <th>Сумма</th>
-//                     <th>Статус</th>
-//                     <th>Дата</th>
-//                 </tr>
-//                 </thead>
-//                 <tbody>
-//                 {orders.map((order) => (
-//                     <tr key={order._id}>
-//                         <td>{order._id}</td>
-//                         <td>{order.user ? order.user.name : 'Гость'}</td>
-//                         <td>
-//                             {order.cart.map((item) => (
-//                                 <p key={item.product._id}>
-//                                     {item.product.name} (Количество: {item.quantity})
-//                                 </p>
-//                             ))}
-//                         </td>
-//                         <td>{order.totalAmount.toFixed(2)} KGS</td>
-//                         <td>{order.status}</td>
-//                         <td>{new Date(order.date).toLocaleString()}</td>
-//                     </tr>
-//                 ))}
-//                 </tbody>
-//             </table>
-//         </div>
-//     );
-// };
-//
-// export default OrderList;
-
-
 
 
 
@@ -185,8 +123,125 @@ import OrderItem from "./OrderItem"; // Подключение стилей
 
 
 
+// const OrderList = () => {
+//     const [orders, setOrders] = useState([]);
+//
+//     useEffect(() => {
+//         const fetchOrders = async () => {
+//             try {
+//                 const response = await fetch('http://localhost:5500/api/orders/orders');
+//                 const data = await response.json();
+//                 setOrders(data);
+//             } catch (error) {
+//                 console.error('Fetch error:', error);
+//             }
+//         };
+//
+//         fetchOrders();
+//     }, []);
+//
+//     const updateStatus = async (orderId, newStatus) => {
+//         try {
+//             const response = await fetch(`http://localhost:5500/api/orders/update-status/${orderId}`, {
+//                 method: 'PUT',
+//                 headers: {
+//                     'Content-Type': 'application/json',
+//                 },
+//                 body: JSON.stringify({ status: newStatus }),
+//             });
+//
+//             if (response.ok) {
+//                 const updatedOrders = orders.map((order) => {
+//                     if (order._id === orderId) {
+//                         return { ...order, status: newStatus, statusHistory: [...order.statusHistory, { status: newStatus, time: Date.now() }] };
+//                     }
+//                     return order;
+//                 });
+//
+//                 setOrders(updatedOrders);
+//             } else {
+//                 console.error('Failed to update status');
+//             }
+//         } catch (error) {
+//             console.error('Error updating status:', error);
+//         }
+//     };
+//
+//     return (
+//         <div className="order">
+//             <h2>Список заказов</h2>
+//             <table>
+//                 <thead>
+//                 <tr>
+//                     <th>Номер заказа</th>
+//                     <th>Клиент</th>
+//                     <th>Имя</th>
+//                     <th>Email</th>
+//                     <th>Адрес</th>
+//                     <th>№ Тел</th>
+//                     <th>Способ опл</th>
+//                     <th>Комментарии</th>
+//                     <th>Товары</th>
+//                     <th>Сумма</th>
+//                     <th>Статус</th>
+//                     <th>Дата изменения статуса</th>
+//                     <th>Дата создания заказа</th>
+//                 </tr>
+//                 </thead>
+//                 <tbody>
+//                 {orders.map((order, index) => (
+//                     <tr key={order._id}>
+//                         <td>{index + 1}</td>
+//                         <td>{order.user ? order.user.role : 'Гость'}</td>
+//                         <td>{order.user ? order.user.name : '-'}</td>
+//                         <td>{order.user ? order.user.email : '-'}</td>
+//                         <td>{order.address ? order.address : '-'}</td>
+//                         <td>{order.phoneNumber ? order.phoneNumber : '-'}</td>
+//                         <td>{order.user ? order.paymentMethod : '-'}</td>
+//                         <td>
+//                                 <textarea
+//                                     style={{ boxSizing: "border-box", fontSize: "12px" }}
+//                                     defaultValue={order.comments ? order.comments : '-'}
+//                                 ></textarea>
+//                         </td>
+//                         <td>
+//                             {order.products.map((item) => (
+//                                 <span key={item.product._id}>
+//                                         {item.product.type}: {item.quantity}шт; <br />
+//                                     </span>
+//                             ))}
+//                         </td>
+//                         <td>{order.totalAmount.toFixed(2)} KGS</td>
+//                         <OrderItem key={order._id} order={order} onUpdateStatus={updateStatus} />
+//                         <td>
+//                             {order.statusHistory && order.statusHistory.length > 0
+//                                 ? new Date(order.statusHistory[order.statusHistory.length - 1].time).toLocaleString()
+//                                 : '-'}
+//                         </td>
+//                         <td>{new Date(order.date).toLocaleString()}</td>
+//                     </tr>
+//                 ))}
+//                 </tbody>
+//             </table>
+//         </div>
+//     );
+// };
+//
+// export default OrderList;
+
+
+
+
+
+
+
+// import React, { useState, useEffect } from 'react';
+import OrderDetailsModal from './OrderDetailsModal'; // Создайте компонент для отображения деталей заказа
+import './OrderList.css';
+
 const OrderList = () => {
     const [orders, setOrders] = useState([]);
+    const [selectedOrder, setSelectedOrder] = useState(null);
 
     useEffect(() => {
         const fetchOrders = async () => {
@@ -229,6 +284,14 @@ const OrderList = () => {
         }
     };
 
+    const handleOrderClick = (order) => {
+        setSelectedOrder(order);
+    };
+
+    const handleCloseModal = () => {
+        setSelectedOrder(null);
+    };
+
     return (
         <div className="order">
             <h2>Список заказов</h2>
@@ -252,7 +315,7 @@ const OrderList = () => {
                 </thead>
                 <tbody>
                 {orders.map((order, index) => (
-                    <tr key={order._id}>
+                    <tr key={order._id} onClick={() => handleOrderClick(order)}>
                         <td>{index + 1}</td>
                         <td>{order.user ? order.user.role : 'Гость'}</td>
                         <td>{order.user ? order.user.name : '-'}</td>
@@ -274,7 +337,8 @@ const OrderList = () => {
                             ))}
                         </td>
                         <td>{order.totalAmount.toFixed(2)} KGS</td>
-                        <OrderItem key={order._id} order={order} onUpdateStatus={updateStatus} />
+                       <OrderItem key={order._id} order={order} onUpdateStatus={updateStatus} />
+
                         <td>
                             {order.statusHistory && order.statusHistory.length > 0
                                 ? new Date(order.statusHistory[order.statusHistory.length - 1].time).toLocaleString()
@@ -285,6 +349,9 @@ const OrderList = () => {
                 ))}
                 </tbody>
             </table>
+            {selectedOrder && (
+                <OrderDetailsModal order={selectedOrder} onClose={handleCloseModal} />
+            )}
         </div>
     );
 };
