6da071120fd2b39f3cda62112456f3d66731d7d4
bakytdeveloper <bakytdeveloper@gmail.com>
2024-01-30 22:35:03 +0600

    Поправил код, по смене статуса заказа

--- client/src/components/AdminPanel/OrderItem.js
+++ client/src/components/AdminPanel/OrderItem.js
@@ -1,3 +1,42 @@
+// // src/components/Admin/OrderItem.js
+// import React, { useState } from 'react';
+//
+// const OrderItem = ({ order, onUpdateStatus }) => {
+//     const [selectedStatus, setSelectedStatus] = useState(order.status);
+//
+//     const handleStatusChange = () => {
+//         // Выполняем обновление статуса на сервере и в локальном состоянии
+//         onUpdateStatus(order._id, selectedStatus);
+//     };
+//
+//     return (
+//         <tr key={order._id}>
+//             <td>
+//                 <select
+//                     value={selectedStatus}
+//                     onChange={(e) => setSelectedStatus(e.target.value)}
+//                 >
+//                     <option value="Pending">Pending</option>
+//                     <option value="Completed">Completed</option>
+//                     <option value="Cancelled">Cancelled</option>
+//                 </select>
+//                 <button className="selectBtn" onClick={handleStatusChange}
+//                         style={{padding: "2px"}}>
+//                     &#10004;</button>
+//                 {/*<button onClick={handleStatusChange}>Изменить</button>*/}
+//             </td>
+//
+//         </tr>
+//     );
+// };
+//
+// export default OrderItem;
+
+
+
+
+
+
 // src/components/Admin/OrderItem.js
 import React, { useState } from 'react';
 
@@ -9,23 +48,25 @@ const OrderItem = ({ order, onUpdateStatus }) => {
         onUpdateStatus(order._id, selectedStatus);
     };
 
+    const handleChange = (e) => {
+        // Обновляем локальное состояние при изменении значения в select
+        setSelectedStatus(e.target.value);
+        // Выполняем обновление статуса на сервере
+        onUpdateStatus(order._id, e.target.value);
+    };
+
     return (
         <tr key={order._id}>
             <td>
                 <select
                     value={selectedStatus}
-                    onChange={(e) => setSelectedStatus(e.target.value)}
+                    onChange={handleChange}
                 >
                     <option value="Pending">Pending</option>
                     <option value="Completed">Completed</option>
                     <option value="Cancelled">Cancelled</option>
                 </select>
-                <button className="selectBtn" onClick={handleStatusChange}
-                        style={{padding: "2px"}}>
-                    &#10004;</button>
-                {/*<button onClick={handleStatusChange}>Изменить</button>*/}
             </td>
-
         </tr>
     );
 };
