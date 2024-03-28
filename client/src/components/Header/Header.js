76dcf62d1f700647ff0690612053ec97d6c7c952
bakytdeveloper <bakytdeveloper@gmail.com>
2024-03-28 21:08:15 +0600

    Поправил функционал отображения кнопок в шапке, в адаптивке

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -119,7 +119,7 @@
 
 
 // src/components/Header/Header.js
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import './Header.css';
 import { Link, useHistory } from 'react-router-dom';
 import ins from "./instagram.png";
@@ -131,17 +131,21 @@ import cart from './trolley2.png';
 import burger from './burger.png';
 import ContactInfo from './ContactInfo';
 
-const Header = ({ onSearch, cartItems, showSidebar,
-                    setShowSidebar, selectedOption,
-                    setSelectedOption, resetFilter }) => {
+const Header = ({ onSearch, cartItems, showSidebar, setShowSidebar, selectedOption, setSelectedOption, resetFilter }) => {
     const [searchTerm, setSearchTerm] = useState('');
+    const [catalogButtonColor, setCatalogButtonColor] = useState('initial');
+    const [contactButtonColor, setContactButtonColor] = useState('initial');
     const totalItems = cartItems.reduce((total, item) => total + item.quantity, 0);
     const history = useHistory();
     const [isProfileOpen, setIsProfileOpen] = useState(false); // Добавляем состояние для открытой страницы профиля
-
-
     const isAuthenticated = localStorage.getItem('token'); // Проверка аутентификации
 
+    useEffect(() => {
+        if (showSidebar) {
+            setCatalogButtonColor('initial');
+            setContactButtonColor('initial');
+        }
+    }, [showSidebar]);
 
     const handleSearchChange = (e) => {
         const value = e.target.value;
@@ -162,27 +166,20 @@ const Header = ({ onSearch, cartItems, showSidebar,
         history.push("/");
     };
 
-
     const handleContactClick = () => {
         setSelectedOption('contact');
         setShowSidebar(!showSidebar);
+        setContactButtonColor('lightblue');
+        setCatalogButtonColor('initial');
     };
 
     const handleCatalogClick = () => {
         setSelectedOption('catalog');
         setShowSidebar(!showSidebar);
+        setCatalogButtonColor('lightblue');
+        setContactButtonColor('initial');
     };
 
-    // // Функция для перехода на страницу профиля или страницу логина
-    // const handleProfileClick = () => {
-    //     if (isAuthenticated) {
-    //         history.push("/profile");
-    //     } else {
-    //         history.push("/login");
-    //     }
-    // };
-
-    // Функция для перехода на страницу профиля или страницу логина и открытия/закрытия
     const handleProfileClick = () => {
         if (isAuthenticated) {
             if (!isProfileOpen) {
@@ -196,11 +193,13 @@ const Header = ({ onSearch, cartItems, showSidebar,
         }
     };
 
+    const handleCloseSidebar = () => {
+        setShowSidebar(false);
+    };
 
     return (
         <div className="header">
             <Link to="/" className="title" onClick={handleTitleClick}>
-
                 <h1 className="titleH"> kiosk<span className="titleN">.kg</span></h1>
             </Link>
             {/* Показывать контактную информацию только на больших экранах */}
@@ -208,35 +207,28 @@ const Header = ({ onSearch, cartItems, showSidebar,
                 <ContactInfo />
             </div>
             <div className="search">
-                <input type="text"
-                       placeholder="Поиск...&#128269;"
-                       value={searchTerm}
-                       onChange={handleSearchChange} />
+                <input type="text" placeholder="Поиск...&#128269;" value={searchTerm} onChange={handleSearchChange} />
             </div>
             <div className="auth-buttons">
                 <Link to="/cart" style={{ display: "inline-flex" }} className="auth-button btn" onClick={handleCartClick}>
                     <img src={cart} alt="Cart Icon" />
-                    <span className="totalItems">
-                        ({totalItems})
-                    </span>
+                    <span className="totalItems">({totalItems})</span>
                 </Link>
                 <div className="profileIcon" onClick={handleProfileClick}>
                     <img src={profileIcon} alt="profileIcon" />
                 </div>
             </div>
             <div className="mobile-buttons">
-                <div className="btn1"  onClick={() => { setShowSidebar(!showSidebar); setSelectedOption('catalog'); }}>
+                <div className={`btn1 ${catalogButtonColor === 'lightblue' ? 'blueText' : ''}`} onClick={handleCatalogClick} style={{ backgroundColor: catalogButtonColor }}>
                     <Link to="/catalog" className="btn">Каталог товаров</Link>
                 </div>
-                <div className="btn2" onClick={() => { setShowSidebar(!showSidebar); setSelectedOption('contact'); }}>
-                    <Link to="/contact" className="btn" >Связаться с нами</Link>
+                <div className={`btn2 ${contactButtonColor === 'lightblue' ? 'blueText' : ''}`} onClick={handleContactClick} style={{ backgroundColor: contactButtonColor }}>
+                    <Link to="/contact" className="btn">Связаться с нами</Link>
                 </div>
             </div>
-
+            {/*{showSidebar && <div className="closeBtn" onClick={handleCloseSidebar}>&#215;</div>}*/}
         </div>
     );
 };
 
 export default Header;
-
-
