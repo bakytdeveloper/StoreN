a21aadcfbcb64b3cee9bc87c53c9c05fc4c4a1b4
bakytdeveloper <bakytdeveloper@gmail.com>
2024-04-01 19:34:22 +0600

    Поправил код код функционала для дроп дауна

--- client/src/components/Payment/PaymentForm.js
+++ client/src/components/Payment/PaymentForm.js
@@ -5,7 +5,7 @@ const PaymentForm = () => {
         <div style={{textAlign:"center"}}>
             <h1>Спасибо, ваш заказ принят</h1>
             <h2>Вам перезвонить оператор через несколько минут</h2>
-            <h2 style={{color:"blue"}}>kiosk.kg</h2>
+            <h2 style={{color:"blue"}}>Ожидайте звонка</h2>
         </div>
     );
 };
