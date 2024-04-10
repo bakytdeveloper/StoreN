e0ab65ed368ba297f2d74fa41fecbc4fee86b7ff
bakytdeveloper <bakytdeveloper@gmail.com>
2024-04-10 14:44:12 +0600

    Сделал отображение личных товаров продавцов, в профиле продавца

--- client/src/components/ProductList/ProductList.js
+++ client/src/components/ProductList/ProductList.js
@@ -154,7 +154,6 @@ import right from "./arrowsR.png";
 
 
 
-
 // const ProductList = ({ searchKeyword, cartItems, setCartItems, products,
 //                          setProducts, showSidebar, setShowSidebar  }) => {
 //     const [selectedType, setSelectedType] = useState(null);
@@ -163,9 +162,14 @@ import right from "./arrowsR.png";
 //
 //     const productsPerPage = 10;
 //
+//
 //     useEffect(() => {
 //         window.scrollTo(0, 0); // Scroll to the top of the page when currentPage changes
 //     }, [currentPage]);
+//
+//     useEffect(() => {
+//         setCurrentPage(1); // Reset currentPage to 1 when searchKeyword changes
+//     }, [searchKeyword]);
 //
 //     const fetchProducts = async () => {
 //         try {
@@ -308,58 +312,61 @@ import right from "./arrowsR.png";
 
 
 
+
 const ProductList = ({ searchKeyword, cartItems, setCartItems, products,
                          setProducts, showSidebar, setShowSidebar  }) => {
-    const [selectedType, setSelectedType] = useState(null);
-    const [filteredProducts, setFilteredProducts] = useState([]);
-    const [currentPage, setCurrentPage] = useState(1);
+    const [selectedType, setSelectedType] = useState(null); // Состояние для отслеживания выбранного типа продукта
+    const [filteredProducts, setFilteredProducts] = useState([]); // Состояние для хранения отфильтрованных продуктов
+    const [currentPage, setCurrentPage] = useState(1); // Состояние для отслеживания текущей страницы пагинации
 
-    const productsPerPage = 10;
-
+    const productsPerPage = 10; // Количество продуктов на одной странице
 
     useEffect(() => {
-        window.scrollTo(0, 0); // Scroll to the top of the page when currentPage changes
+        window.scrollTo(0, 0); // Прокрутка страницы наверх при изменении currentPage
     }, [currentPage]);
 
     useEffect(() => {
-        setCurrentPage(1); // Reset currentPage to 1 when searchKeyword changes
+        setCurrentPage(1); // Сброс currentPage до 1 при изменении searchKeyword
     }, [searchKeyword]);
 
+    // Асинхронная функция для получения продуктов с сервера с учетом параметров поиска
     const fetchProducts = async () => {
         try {
-            const response = await fetch(`${process.env.REACT_APP_API_URL}/api/products?search=${searchKeyword}`); // Добавляем параметр запроса для поиска
+            const response = await fetch(`${process.env.REACT_APP_API_URL}/api/products?search=${searchKeyword}`);
             const data = await response.json();
-            setFilteredProducts(filterProducts(data || []));
+            setFilteredProducts(filterProducts(data || [])); // Фильтрация полученных продуктов
         } catch (error) {
             console.error('Error fetching products:', error);
         }
     };
 
+    // Эффект для загрузки продуктов при изменении searchKeyword, selectedType, products, currentPage
     useEffect(() => {
         if (products && products.length > 0) {
-            setFilteredProducts(filterProducts(products));
+            setFilteredProducts(filterProducts(products)); // Фильтрация продуктов из состояния, если они уже загружены
         } else {
-            fetchProducts();
+            fetchProducts(); // Иначе, загрузка продуктов с сервера
         }
     }, [searchKeyword, selectedType, products, currentPage]);
 
-
+    // Функция фильтрации продуктов
     const filterProducts = (productsToFilter) => {
         return productsToFilter
-            .filter((product) => !selectedType || product.type === selectedType)
+            .filter((product) => !selectedType || product.type === selectedType) // Фильтрация по типу продукта
             .filter(
                 (product) =>
                     searchKeyword
                         ? product.name.toLowerCase().includes(searchKeyword.toLowerCase()) ||
                         product.description.toLowerCase().includes(searchKeyword.toLowerCase()) ||
                         product.brand.toLowerCase().includes(searchKeyword.toLowerCase()) ||
-                        product.type.toLowerCase().includes(searchKeyword.toLowerCase())
+                        product.type.toLowerCase().includes(searchKeyword.toLowerCase()) // Фильтрация по ключевому слову поиска
                         : true
             );
     };
 
     const history = useHistory();
 
+    // Обработчик добавления продукта в корзину
     const handleAddToCart = (product) => {
         const itemInCart = cartItems.find((item) => item.productId === product._id);
 
@@ -383,11 +390,13 @@ const ProductList = ({ searchKeyword, cartItems, setCartItems, products,
         }
     };
 
+    // Обработчик немедленной покупки продукта
     const handleBuyNow = (product) => {
-        handleAddToCart(product);
-        history.push('/cart');
+        handleAddToCart(product); // Добавление продукта в корзину
+        history.push('/cart'); // Переход на страницу корзины
     };
 
+    // Обработчики для перехода к предыдущей и следующей страницам пагинации
     const handleNextPage = () => {
         setCurrentPage((prevPage) => prevPage + 1);
     };
@@ -396,14 +405,14 @@ const ProductList = ({ searchKeyword, cartItems, setCartItems, products,
         setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
     };
 
+    // Вычисление общего количества страниц и индекса начала отображаемых продуктов на текущей странице
     const totalPages = Math.ceil(filteredProducts.length / productsPerPage);
     const startIndex = (currentPage - 1) * productsPerPage;
     const displayedProducts = filteredProducts.slice(startIndex, startIndex + productsPerPage);
 
     // Функция для исправления пути к изображениям
     const fixImagePath = (imagePath) => {
-        // Удаляем лишнюю часть пути "images/W/MEDIAX_792452-T2/"
-        return imagePath.replace("images/W/MEDIAX_792452-T2/", "");
+        return imagePath.replace("images/W/MEDIAX_792452-T2/", ""); // Удаление лишней части пути к изображениям
     };
 
     return (
@@ -454,7 +463,6 @@ const ProductList = ({ searchKeyword, cartItems, setCartItems, products,
                 <span className="numStr">{`Страница ${currentPage} из ${totalPages}`}</span>
                 <button  className="arrowR" onClick={handleNextPage} disabled={currentPage === totalPages}>
                     <img  className="arrowRImg" src={right}  alt="Cart" />
-
                 </button>
             </div>
         </div>
@@ -462,5 +470,6 @@ const ProductList = ({ searchKeyword, cartItems, setCartItems, products,
 };
 
 export default ProductList;
+
 
 
--- client/src/components/SellerProductsPage/SellerProductsPage.js
+++ client/src/components/SellerProductsPage/SellerProductsPage.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
+import bas from './../ProductList/basket.png';
 
 const SellerProductsPage = () => {
     const [sellerProducts, setSellerProducts] = useState([]);
@@ -33,6 +34,12 @@ const SellerProductsPage = () => {
         history.goBack(); // Переход на предыдущую страницу
     };
 
+
+    // Функция для исправления пути к изображениям
+    const fixImagePath = (imagePath) => {
+        return imagePath.replace("images/W/MEDIAX_792452-T2/", ""); // Удаление лишней части пути к изображениям
+    };
+
     return (
         <div style={{marginTop:"110px"}}>
             <h1>Мои товары</h1>
@@ -40,14 +47,41 @@ const SellerProductsPage = () => {
                 Назад к профилю
             </button>
 
-            <div>
+            <div className="product-list">
                 {/* Здесь отображаем информацию о товарах */}
                 {sellerProducts.map((product) => (
-                    <div key={product._id}>
-                        <h3>{product.name}</h3>
-                        <p>Описание: {product.description}</p>
-                        <p>Цена: {product.price}</p>
-                        {/* Другие детали товара */}
+                    <div className="product-card" key={product._id}>
+                        <Link to={`/products/${product._id}`}>
+                            <img
+                                src={product.images && product.images.length > 0 ? fixImagePath(product.images[0]) : 'placeholder.jpg'}
+                                alt={product.name}
+                            />
+                            <div className="details">
+                                <div className="type">{product.type}</div>
+                                <div className="brand">{product.brand}</div>
+                                <div className="name">{product.name}</div>
+                                <div className="price">
+                                    <span>KGS</span> {product.price}
+                                </div>
+                            </div>
+                        </Link>
+                        <div className="actions">
+                            <button
+                                className="cart-button"
+                                title="Add to Cart"
+                                // onClick={() => handleAddToCart(product)}
+                            >
+                                <strong>+</strong>
+                                <img style={{ width: '26px', height: '26px' }} src={bas} alt="Cart" />
+                            </button>
+                            <button
+                                className="buy-button"
+                                title="Buy Now"
+                                // onClick={() => handleBuyNow(product)}
+                            >
+                                Заказать
+                            </button>
+                        </div>
                     </div>
                 ))}
             </div>
@@ -56,3 +90,4 @@ const SellerProductsPage = () => {
 };
 
 export default SellerProductsPage;
+
