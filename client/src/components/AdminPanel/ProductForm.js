b157fa248608e7436e900fb0330bdb482bc7aa5b
bakytdeveloper <bakytdeveloper@gmail.com>
2024-05-04 01:42:00 +0600

    Сделал обязательные поля, при создании товара продавцом

--- client/src/components/AdminPanel/ProductForm.js
+++ client/src/components/AdminPanel/ProductForm.js
@@ -4,247 +4,6 @@ import './ProductForm.css';
 import {toast} from "react-toastify";
 import {useHistory, useLocation, useParams} from "react-router-dom";
 
-// const ProductForm = ({ onSubmit, onCancel }) => {
-//     const [selectedProduct, setSelectedProduct] = useState(null);
-//     const [products, setProducts] = useState([]);
-//     const { productId } = useParams();
-//     const [productData, setProductData] = useState(null);
-//
-//     const location = useLocation();
-//     const { product } = location.state || {};
-//     const [formData, setFormData] = useState({
-//         name: '',
-//         description: '',
-//         price: "",
-//         category: '',
-//         type: '',
-//         brand: '',
-//         characteristics: [],
-//         images: [],
-//     });
-//
-//
-//
-//     useEffect(() => {
-//         const fetchProduct = async () => {
-//             try {
-//                 const response = await fetch(`${process.env.REACT_APP_API_URL}/api/products/${productId}`);
-//                 if (response.ok) {
-//                     const data = await response.json();
-//                     setProductData(data);
-//                 } else {
-//                     console.error('Failed to fetch product data');
-//                 }
-//             } catch (error) {
-//                 console.error('Error fetching product data:', error);
-//             }
-//         };
-//
-//         fetchProduct();
-//     }, [productId]);
-//
-//
-//
-//
-//     useEffect(() => {
-//         if (product) {
-//             setFormData(product);
-//         }
-//     }, [product]);
-//
-//     const handleChange = (e) => {
-//         setFormData({ ...formData, [e.target.name]: e.target.value });
-//     };
-//
-//     const handleCharacteristicChange = (index, field, value) => {
-//         const updatedCharacteristics = [...formData.characteristics];
-//         updatedCharacteristics[index][field] = value;
-//         setFormData({ ...formData, characteristics: updatedCharacteristics });
-//     };
-//
-//     const handleImageChange = (index, value) => {
-//         const updatedImages = [...formData.images];
-//         updatedImages[index] = value;
-//         setFormData({ ...formData, images: updatedImages });
-//     };
-//
-//
-//     const handleFormSubmit = async (formData) => {
-//         try {
-//             let response;
-//             const token = localStorage.getItem('token');
-//
-//             // Получаем список товаров этого продавца
-//             const sellerProductsResponse = await fetch(`${process.env.REACT_APP_API_URL}/api/sellers/products`, {
-//                 method: 'GET',
-//                 headers: {
-//                     'Authorization': `Bearer ${token}`,
-//                 },
-//             });
-//
-//             if (!sellerProductsResponse.ok) {
-//                 console.error('Error fetching seller products:', sellerProductsResponse.statusText);
-//                 return;
-//             }
-//
-//             const sellerProducts = await sellerProductsResponse.json();
-//
-//             // Проверяем, создается ли новый товар
-//             if (!productId) {
-//                 // Проверяем, есть ли уже товар с таким же названием и брендом
-//                 const existingProduct = sellerProducts.find(
-//                     (product) =>
-//                         product.name === formData.name && product.brand === formData.brand
-//                 );
-//
-//                 if (existingProduct) {
-//                     // Если товар уже существует, показываем оповещение и выходим из функции
-//                     toast.error('Такой товар уже существует');
-//                     return;
-//                 }
-//             }
-//
-//             if (productId) {
-//                 // Если есть productId, отправляем запрос на обновление существующего товара
-//                 response = await fetch(
-//                     `${process.env.REACT_APP_API_URL}/api/sellers/products/${productId}`,
-//                     {
-//                         method: 'PUT',
-//                         headers: {
-//                             'Content-Type': 'application/json',
-//                             Authorization: `Bearer ${token}`,
-//                         },
-//                         body: JSON.stringify(formData),
-//                     }
-//                 );
-//             } else {
-//                 // Если нет productId, отправляем запрос на создание нового товара
-//                 response = await fetch(
-//                     `${process.env.REACT_APP_API_URL}/api/sellers/products`,
-//                     {
-//                         method: 'POST',
-//                         headers: {
-//                             'Content-Type': 'application/json',
-//                             Authorization: `Bearer ${token}`,
-//                         },
-//                         body: JSON.stringify(formData),
-//                     }
-//                 );
-//             }
-//
-//             if (response.ok) {
-//                 clearFormFields();
-//                 const updatedProduct = await response.json();
-//                 setProducts((prevProducts) =>
-//                     prevProducts.map((product) =>
-//                         product._id === updatedProduct._id ? updatedProduct : product
-//                     )
-//                 );
-//                 setSelectedProduct(null);
-//                 toast.success(productId ? 'Товар успешно обновлен!' : 'Товар успешно создан!');
-//             } else {
-//                 console.error('Failed to save product');
-//             }
-//         } catch (error) {
-//             console.error('Error saving product:', error);
-//         }
-//     };
-//
-//
-//
-//
-//
-//
-//
-//
-//     const clearFormFields = () => {
-//         setFormData({
-//             name: '',
-//             description: '',
-//             price: '',
-//             category: '',
-//             type: '',
-//             brand: '',
-//             characteristics: [],
-//             images: [],
-//             quantity: 10
-//         });
-//     };
-//
-//     const handleSubmit = (e) => {
-//         e.preventDefault();
-//         // onSubmit(formData);
-//     };
-//
-//     return (
-//         <form className="sellerFormAdd" onSubmit={handleSubmit} >
-//             <h2>Добавить товар</h2>
-//             <label>Категория:</label>
-//             <input type="text" name="category" value={formData.category} onChange={handleChange} required />
-//
-//             <label>Тип:</label>
-//             <input type="text" name="type" value={formData.type} onChange={handleChange} required />
-//
-//             <label>Бренд:</label>
-//             <input type="text" name="brand" value={formData.brand} onChange={handleChange} required />
-//
-//
-//             <label>Название:</label>
-//             <input type="text" name="name" value={formData.name} onChange={handleChange} required />
-//
-//             <label>Описание:</label>
-//             <textarea name="description" value={formData.description} onChange={handleChange} required />
-//
-//             <label>Цена:</label>
-//             <input type="number" placeholder="0" name="price" value={formData.price} onChange={handleChange} required />
-//
-//             <label>Характеристики:</label>
-//             {formData.characteristics && formData.characteristics.map((char, index) => (
-//                 <div key={index}>
-//                     <input
-//                         type="text"
-//                         value={char.name}
-//                         onChange={(e) => handleCharacteristicChange(index, 'name', e.target.value)}
-//                         placeholder="Название характеристики"
-//                     />
-//                     <input
-//                         type="text"
-//                         value={char.value}
-//                         onChange={(e) => handleCharacteristicChange(index, 'value', e.target.value)}
-//                         placeholder="Значение характеристики"
-//                     />
-//                 </div>
-//             ))}
-//             <button  className="newProductAdd"  type="button" onClick={() => setFormData({ ...formData, characteristics: [...formData.characteristics, { name: '', value: '' }] })}>
-//                 Добавить характеристику
-//             </button>
-//
-//             <label>URL картинки:</label>
-//             {formData.images.map((image, index) => (
-//                 <div key={index}>
-//                     <input
-//                         type="text"
-//                         value={image}
-//                         onChange={(e) => handleImageChange(index, e.target.value)}
-//                         placeholder="URL картинки"
-//                     />
-//                 </div>
-//             ))}
-//             <button  className="newProductAdd"  type="button" onClick={() => setFormData({ ...formData, images: [...formData.images, ''] })}>
-//                 Добавить изображение
-//             </button>
-//         <div className="submitBtn">
-//             <button className="submit" onClick={() => handleFormSubmit(formData)}>&#10004; Создать продукт</button>
-//             <button className="cancel" type="button" onClick={onCancel}>
-//                 &#10006;  Отмена
-//             </button>
-//         </div>
-//
-//         </form>
-//     );
-// };
-//
-// export default ProductForm;
 
 
 const ProductForm = ({ onSubmit, onCancel }) => {
@@ -379,6 +138,7 @@ const ProductForm = ({ onSubmit, onCancel }) => {
     };
 
     const handleFormSubmit = async (formData) => {
+
         try {
             let response;
             const token = localStorage.getItem('token');
@@ -475,6 +235,18 @@ const ProductForm = ({ onSubmit, onCancel }) => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+
+        // Проверяем, чтобы хотя бы одно поле характеристик имело заполненное значение
+        const hasValidCharacteristics = formData.characteristics.some(char => char.name.trim() !== '' && char.value.trim() !== '');
+
+        // Проверяем, чтобы хотя бы одно поле картинок имело заполненное значение
+        const hasValidImages = formData.images.some(image => image.trim() !== '');
+
+        if (!hasValidCharacteristics || !hasValidImages) {
+            toast.error('Характеристики и изображения должны быть заполнены хотя бы одним значением');
+            return;
+        }
+
         handleFormSubmit(formData);
     };
 
@@ -523,12 +295,14 @@ const ProductForm = ({ onSubmit, onCancel }) => {
                         value={char.name}
                         onChange={(e) => handleCharacteristicChange(index, 'name', e.target.value)}
                         placeholder="Название характеристики"
+                        required
                     />
                     <input
                         type="text"
                         value={char.value}
                         onChange={(e) => handleCharacteristicChange(index, 'value', e.target.value)}
                         placeholder="Значение характеристики"
+                        required
                     />
                 </div>
             ))}
@@ -544,6 +318,7 @@ const ProductForm = ({ onSubmit, onCancel }) => {
                         value={image}
                         onChange={(e) => handleImageChange(index, e.target.value)}
                         placeholder="URL картинки"
+                        required
                     />
                 </div>
             ))}
