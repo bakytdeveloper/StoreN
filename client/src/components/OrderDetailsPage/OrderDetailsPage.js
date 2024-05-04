184932f6b350470f29d738fff70bff53fb229650
bakytdeveloper <bakytdeveloper@gmail.com>
2024-05-04 21:02:24 +0600

    Начинаю поправку кода в админ панеле для изменения количества товара

--- client/src/components/OrderDetailsPage/OrderDetailsPage.js
+++ client/src/components/OrderDetailsPage/OrderDetailsPage.js
@@ -1,21 +1,149 @@
+//
+//
+// import React from 'react';
+// import { useParams } from 'react-router-dom';
+// import OrderDetailsModal from '../AdminPanel/OrderDetailsModal';
+//
+// const OrderDetailsPage = ({ orders, onUpdateQuantity, onDeleteItem }) => {
+//     const { orderId } = useParams();
+//     const order = orders.find(order => order._id === orderId);
+//
+//     return (
+//         <div className="order-details-page">
+//             {order && (
+//                 <OrderDetailsModal
+//                     order={order}
+//                     onUpdateQuantity={onUpdateQuantity}
+//                     onDeleteItem={onDeleteItem}
+//                 />
+//             )}
+//         </div>
+//     );
+// };
+//
+// export default OrderDetailsPage;
+//
 
 
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import OrderDetailsModal from '../AdminPanel/OrderDetailsModal';
+import React, { useState, useEffect } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
 
 const OrderDetailsPage = ({ orders, onUpdateQuantity, onDeleteItem }) => {
     const { orderId } = useParams();
     const order = orders.find(order => order._id === orderId);
 
+    const [totalAmount, setTotalAmount] = useState(order ? order.totalAmount : 0);
+
+    useEffect(() => {
+        if (order) {
+            setTotalAmount(order.totalAmount);
+        }
+    }, [order]);
+
+    const history = useHistory();
+
+    const onClose = () => {
+        history.goBack(); // Переход на предыдущую страницу
+    };
+
     return (
         <div className="order-details-page">
             {order && (
-                <OrderDetailsModal
-                    order={order}
-                    onUpdateQuantity={onUpdateQuantity}
-                    onDeleteItem={onDeleteItem}
-                />
+                <div className="order-details-modal">
+                    <div className="modal-content">
+                        <button className="close-button" onClick={onClose}>
+                            &#10006;
+                        </button>
+                        <h2>Детали заказа</h2>
+                        <div className="order-info">
+                            <div>
+                                <strong>ID заказа:</strong> {order._id}
+                            </div>
+                            <div>
+                                <strong>Клиент:</strong> {order.user ? order.user.name : 'Гость'}
+                            </div>
+                            <div>
+                                <strong>Email:</strong> {order.user ? order.user.email : '-'}
+                            </div>
+                            <div>
+                                <strong>Адрес:</strong> {order.address ? order.address : '-'}
+                            </div>
+                            <div>
+                                <strong>Телефон:</strong> {order.phoneNumber ? order.phoneNumber : '-'}
+                            </div>
+                            <div>
+                                <strong>Способ оплаты:</strong> {order.user ? order.paymentMethod : '-'}
+                            </div>
+                            <div>
+                                <strong>Комментарии:</strong> {order.comments ? order.comments : '-'}
+                            </div>
+                            <div>
+                                <strong>Статус:</strong> {order.status}
+                            </div>
+                            <div>
+                                <strong>Дата изменения статуса:</strong>{' '}
+                                {order.statusHistory && order.statusHistory.length > 0
+                                    ? new Date(order.statusHistory[order.statusHistory.length - 1].time).toLocaleString()
+                                    : '-'}
+                            </div>
+                            <div>
+                                <strong>Дата создания заказа:</strong> {new Date(order.date).toLocaleString()}
+                            </div>
+                            <div>
+                                <strong>Товары:</strong>
+                                <hr />
+                                <ul>
+                                    {order.products.map((item, index) => (
+                                        <li key={item.product._id}>
+                                            {item.product && (
+                                                <div>
+                                                    <strong>
+                                                        {' '}
+                                                        <span>{index + 1})</span> Тип товара:
+                                                    </strong>{' '}
+                                                    {item.product.type}
+                                                </div>
+                                            )}
+                                            {item.product && (
+                                                <div>
+                                                    <strong>Бренд:</strong> {item.product.brand}
+                                                </div>
+                                            )}
+                                            {item.product && (
+                                                <div>
+                                                    <strong>Название товара:</strong> {item.product.name}
+                                                </div>
+                                            )}
+                                            {item.product && (
+                                                <div>
+                                                    <strong>Описание:</strong> {item.product.description}
+                                                </div>
+                                            )}
+                                            <div className="quantityItem">
+                                                <strong>Количество:</strong> {item.quantity}
+                                                <div className="quantityButtons">
+                                                    <button className="minusQuantityButton" onClick={() => onUpdateQuantity(order._id, item.product._id, item.quantity - 1)}>-</button>
+                                                    <button className="plusQuantityButton"  onClick={() => onUpdateQuantity(order._id, item.product._id, item.quantity + 1)}>+</button>
+                                                </div>
+                                                <button className="deleteOneItemOrder" onClick={() => onDeleteItem(order._id, item.product._id)}>Удалить</button>
+                                            </div>
+                                            {item.product && (
+                                                <div>
+                                                    <strong>Цена за единицу: {item.product.price}</strong> KGS
+                                                </div>
+                                            )}
+                                            <hr />
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                            <div>
+                                <strong>Общая сумма заказа: {totalAmount}</strong> KGS
+                                {/*<strong>Общая сумма заказа:</strong> {totalAmount.toFixed(2)} KGS*/}
+                            </div>
+                        </div>
+                    </div>
+                </div>
             )}
         </div>
     );
