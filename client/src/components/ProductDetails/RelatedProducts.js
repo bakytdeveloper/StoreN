4de4a1aed030387071d4ef6848dda016b9cfe880
bakytdeveloper <bakytdeveloper@gmail.com>
2024-05-09 12:12:27 +0600

    Сделал скрол на верх при выборе товара, на странице деталей товара

--- client/src/components/ProductDetails/RelatedProducts.js
+++ client/src/components/ProductDetails/RelatedProducts.js
@@ -59,6 +59,110 @@
 
 
 
+// import React, { useState, useEffect } from 'react';
+// import { Link } from 'react-router-dom';
+//
+// const RelatedProducts = ({ productId }) => {
+//     const [relatedProducts, setRelatedProducts] = useState([]);
+//     const [currentIndex, setCurrentIndex] = useState(0);
+//     const [cardCount, setCardCount] = useState(5); // Количество карточек по умолчанию
+//
+//     useEffect(() => {
+//         const fetchRelatedProducts = async () => {
+//             try {
+//                 const response = await fetch(`${process.env.REACT_APP_API_URL}/api/products/related/${productId}`);
+//                 if (response.ok) {
+//                     const data = await response.json();
+//                     setRelatedProducts(data);
+//                 } else {
+//                     console.error('Error fetching related products:', response.statusText);
+//                 }
+//             } catch (error) {
+//                 console.error('Error fetching related products:', error);
+//             }
+//         };
+//
+//         fetchRelatedProducts();
+//     }, [productId]);
+//
+//     useEffect(() => {
+//         const handleResize = () => {
+//             const screenWidth = window.innerWidth;
+//             let count = 5; // Количество карточек по умолчанию
+//
+//             if (screenWidth <= 1100) {
+//                 count = 4; // Изменить количество карточек для экранов <= 1100px
+//             }
+//
+//             if (screenWidth <= 768) {
+//                 count = 3; // Изменить количество карточек для экранов <= 768px
+//             }
+//
+//             setCardCount(count);
+//         };
+//
+//         window.addEventListener('resize', handleResize);
+//
+//         return () => {
+//             window.removeEventListener('resize', handleResize);
+//         };
+//     }, []);
+//
+//     const handlePrevClick = () => {
+//         setCurrentIndex(prevIndex => Math.max(0, prevIndex - cardCount));
+//     };
+//
+//     const handleNextClick = () => {
+//         setCurrentIndex(prevIndex => Math.min(relatedProducts.length - cardCount, prevIndex + cardCount));
+//     };
+//
+//
+//     const fixImagePath = (imagePath) => {
+//         return imagePath.replace("images/W/MEDIAX_792452-T2/", "");
+//     };
+//
+//
+//     return (
+//         <div className="related-products">
+//             <h2>Похожие товары</h2>
+//             <div className="products-lists">
+//                 {relatedProducts.slice(currentIndex, currentIndex + cardCount).map((product) => (
+//                     <div className="product-cards" key={product._id}>
+//                         <Link to={`/products/${product._id}`}>
+//                             <img
+//                                 src={product.images && product.images.length > 0 ? fixImagePath(product.images[0]) : 'placeholder.jpg'}
+//                                 // src={product.images && product.images.length > 0 ? product.images[0] : 'placeholder.jpg'}
+//                                 alt={product.name}
+//                             />
+//                             <div className="details">
+//                                 <div className="type">{product.type}</div>
+//                                 <div className="brand">{product.brand}</div>
+//                                 <div className="name">{product.name}</div>
+//                                 {/*<div className="price">*/}
+//                                 {/*    <span>KGS</span> {product.price}*/}
+//                                 {/*</div>*/}
+//                             </div>
+//                         </Link>
+//                     </div>
+//                 ))}
+//             </div>
+//             <div className="slider-controls">
+//                 <button onClick={handlePrevClick} disabled={currentIndex === 0}>
+//                     &#8592; Назад
+//                 </button>
+//                 <button onClick={handleNextClick}
+//                         disabled={currentIndex + cardCount >= relatedProducts.length}>
+//                     Вперёд &#8594;
+//                 </button>
+//             </div>
+//         </div>
+//     );
+// };
+//
+// export default RelatedProducts;
+
+
+
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
@@ -116,11 +220,13 @@ const RelatedProducts = ({ productId }) => {
         setCurrentIndex(prevIndex => Math.min(relatedProducts.length - cardCount, prevIndex + cardCount));
     };
 
+    const handleCardClick = () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' }); // Прокрутка к верху страницы с плавным эффектом
+    };
 
     const fixImagePath = (imagePath) => {
         return imagePath.replace("images/W/MEDIAX_792452-T2/", "");
     };
-
 
     return (
         <div className="related-products">
@@ -128,10 +234,9 @@ const RelatedProducts = ({ productId }) => {
             <div className="products-lists">
                 {relatedProducts.slice(currentIndex, currentIndex + cardCount).map((product) => (
                     <div className="product-cards" key={product._id}>
-                        <Link to={`/products/${product._id}`}>
+                        <Link to={`/products/${product._id}`} onClick={handleCardClick}>
                             <img
                                 src={product.images && product.images.length > 0 ? fixImagePath(product.images[0]) : 'placeholder.jpg'}
-                                // src={product.images && product.images.length > 0 ? product.images[0] : 'placeholder.jpg'}
                                 alt={product.name}
                             />
                             <div className="details">
@@ -150,8 +255,7 @@ const RelatedProducts = ({ productId }) => {
                 <button onClick={handlePrevClick} disabled={currentIndex === 0}>
                     &#8592; Назад
                 </button>
-                <button onClick={handleNextClick}
-                        disabled={currentIndex + cardCount >= relatedProducts.length}>
+                <button onClick={handleNextClick} disabled={currentIndex + cardCount >= relatedProducts.length}>
                     Вперёд &#8594;
                 </button>
             </div>
--- client/src/components/ProductDetails/RelatedSellerProducts.js
+++ client/src/components/ProductDetails/RelatedSellerProducts.js
@@ -1,3 +1,106 @@
+// import React, { useState, useEffect } from 'react';
+// import { Link } from 'react-router-dom';
+//
+// const RelatedSellerProducts = ({ productId }) => {
+//     const [sellerProducts, setSellerProducts] = useState([]);
+//     const [currentIndex, setCurrentIndex] = useState(0);
+//     const [cardCount, setCardCount] = useState(5); // Количество карточек по умолчанию
+//
+//     useEffect(() => {
+//         const fetchRelatedSellerProducts = async () => {
+//             try {
+//                 const response = await fetch(`${process.env.REACT_APP_API_URL}/api/products/${productId}/seller/products`);
+//                 if (response.ok) {
+//                     const data = await response.json();
+//                     setSellerProducts(data);
+//                 } else {
+//                     console.error('Error fetching related seller products:', response.statusText);
+//                 }
+//             } catch (error) {
+//                 console.error('Error fetching related seller products:', error);
+//             }
+//         };
+//
+//         fetchRelatedSellerProducts();
+//     }, [productId]);
+//
+//     useEffect(() => {
+//         const handleResize = () => {
+//             const screenWidth = window.innerWidth;
+//             let count = 5; // Количество карточек по умолчанию
+//
+//             if (screenWidth <= 1100) {
+//                 count = 4; // Изменить количество карточек для экранов <= 1100px
+//             }
+//
+//             if (screenWidth <= 960) {
+//                 count = 3; // Изменить количество карточек для экранов <= 960px
+//             }
+//
+//             setCardCount(count);
+//         };
+//
+//         window.addEventListener('resize', handleResize);
+//
+//         return () => {
+//             window.removeEventListener('resize', handleResize);
+//         };
+//     }, []);
+//
+//     const fixImagePath = (imagePath) => {
+//         return imagePath.replace("images/W/MEDIAX_792452-T2/", "");
+//     };
+//
+//     const handlePrevClick = () => {
+//         setCurrentIndex(prevIndex => Math.max(0, prevIndex - cardCount));
+//     };
+//
+//     const handleNextClick = () => {
+//         setCurrentIndex(prevIndex => Math.min(sellerProducts.length - cardCount, prevIndex + cardCount));
+//     };
+//
+//     return (
+//         <div className="related-seller-products">
+//             <h2>Другие товары продавца</h2>
+//             <div className="products-lists">
+//                 {sellerProducts.slice(currentIndex, currentIndex + cardCount).map((product, index) => (
+//                     <div className="product-cards" key={product._id}>
+//                         <Link to={`/products/${product._id}`}>
+//                             <img
+//                                 src={product.images && product.images.length > 0 ? fixImagePath(product.images[0]) : 'placeholder.jpg'}
+//                                 alt={product.name}
+//                             />
+//                             <div className="details">
+//                                 <div className="type">{product.type}</div>
+//                                 <div className="brand">{product.brand}</div>
+//                                 <div className="name">{product.name}</div>
+//                                 {/*<div className="price">*/}
+//                                 {/*    <span>KGS</span> {product.price}*/}
+//                                 {/*</div>*/}
+//                             </div>
+//                         </Link>
+//                     </div>
+//                 ))}
+//             </div>
+//             <div className="slider-controls">
+//                 <button className="slider-control-one-left"
+//                         onClick={handlePrevClick} disabled={currentIndex === 0}>
+//                     &#8592; Назад
+//                 </button>
+//                 <button className="slider-control-one-right"
+//                         onClick={handleNextClick}
+//                         disabled={currentIndex + cardCount >= sellerProducts.length}>
+//                     Вперёд &#8594;
+//                 </button>
+//             </div>
+//         </div>
+//     );
+// };
+//
+// export default RelatedSellerProducts;
+
+
+
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
@@ -59,13 +162,17 @@ const RelatedSellerProducts = ({ productId }) => {
         setCurrentIndex(prevIndex => Math.min(sellerProducts.length - cardCount, prevIndex + cardCount));
     };
 
+    const handleCardClick = () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' }); // Прокрутка к верху страницы с плавным эффектом
+    };
+
     return (
         <div className="related-seller-products">
             <h2>Другие товары продавца</h2>
             <div className="products-lists">
                 {sellerProducts.slice(currentIndex, currentIndex + cardCount).map((product, index) => (
                     <div className="product-cards" key={product._id}>
-                        <Link to={`/products/${product._id}`}>
+                        <Link to={`/products/${product._id}`} onClick={handleCardClick}>
                             <img
                                 src={product.images && product.images.length > 0 ? fixImagePath(product.images[0]) : 'placeholder.jpg'}
                                 alt={product.name}
