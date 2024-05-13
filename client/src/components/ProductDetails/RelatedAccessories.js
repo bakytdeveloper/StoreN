2ee41bc4a6d13454bc2889c1a6540db420351f62
bakytdeveloper <bakytdeveloper@gmail.com>
2024-05-13 22:20:42 +0600

    Поправил код и подготовил к проде

--- client/src/components/ProductDetails/RelatedAccessories.js
+++ client/src/components/ProductDetails/RelatedAccessories.js
@@ -10,88 +10,6 @@ import sliderRight from './sliderRight.png';
 
 
 
-// const RelatedAccessories = ({ direction }) => {
-//     const [accessories, setAccessories] = useState([]);
-//     const [currentIndex, setCurrentIndex] = useState(0);
-//     const [isLoading, setIsLoading] = useState(false);
-//
-//     useEffect(() => {
-//         const fetchAccessories = async () => {
-//             try {
-//                 const response = await fetch(`${process.env.REACT_APP_API_URL}/api/products/accessories/${direction}`);
-//                 if (response.ok) {
-//                     const data = await response.json();
-//                     setAccessories(data);
-//                 } else {
-//                     console.error('Error fetching related accessories:', response.statusText);
-//                 }
-//             } catch (error) {
-//                 console.error('Error fetching related accessories:', error);
-//             }
-//         };
-//
-//         fetchAccessories();
-//     }, [direction]);
-//
-//     useEffect(() => {
-//         const handleScroll = () => {
-//             if (
-//                 window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight
-//             ) {
-//                 // Пользователь достиг конца страницы, загружаем все доступные аксессуары
-//                 loadAllAccessories();
-//             }
-//         };
-//
-//         window.addEventListener('scroll', handleScroll);
-//
-//         return () => {
-//             window.removeEventListener('scroll', handleScroll);
-//         };
-//     }, []);
-//
-//     const loadAllAccessories = () => {
-//         setIsLoading(true);
-//         setCurrentIndex(accessories.length);
-//     };
-//
-//     const handleCardClick = () => {
-//         document.documentElement.scrollTop = 0;
-//     };
-//
-//     const fixImagePath = (imagePath) => {
-//         return imagePath.replace("images/W/MEDIAX_792452-T2/", "");
-//     };
-//
-//     return (
-//         <div className="related-accessories">
-//             <h2>Аксессуары</h2>
-//             <div className="products-list-related-accessories">
-//                 {accessories.map((accessory, index) => (
-//                     <div className="product-card-related-accessories" key={accessory._id}>
-//                         <Link to={`/products/${accessory._id}`} onClick={handleCardClick}>
-//                             <img
-//                                 src={accessory.images && accessory.images.length > 0 ? fixImagePath(accessory.images[0]) : 'placeholder.jpg'}
-//                                 alt={accessory.name}
-//                             />
-//                             <div className="details-related-accessories">
-//                                 <div className="type-related-accessories">{accessory.type}</div>
-//                                 <div className="brand-related-accessories">{accessory.brand}</div>
-//                                 <div className="name-related-accessories">{accessory.name}</div>
-//                             </div>
-//                         </Link>
-//                     </div>
-//                 ))}
-//                 {isLoading && <p>Loading...</p>}
-//             </div>
-//         </div>
-//     );
-// };
-//
-// export default RelatedAccessories;
-
-
-
 const RelatedAccessories = ({ direction }) => {
     const [accessories, setAccessories] = useState([]);
     const [currentIndex, setCurrentIndex] = useState(0);
@@ -163,39 +81,6 @@ const RelatedAccessories = ({ direction }) => {
     const fixImagePath = (imagePath) => {
         return imagePath.replace("images/W/MEDIAX_792452-T2/", "");
     };
-
-    // return (
-    //     <div className="related-accessories">
-    //         <h2>Аксессуары</h2>
-    //         <div className="products-list-related-accessories">
-    //             {accessories.slice(currentIndex, currentIndex + cardCount).map((accessory, index) => (
-    //                 <div className="product-card-related-accessories" key={accessory._id}>
-    //                     <Link to={`/products/${accessory._id}`} onClick={handleCardClick}>
-    //                         <img
-    //                             src={accessory.images && accessory.images.length > 0 ? fixImagePath(accessory.images[0]) : 'placeholder.jpg'}
-    //                             alt={accessory.name}
-    //                         />
-    //                         <div className="details-related-accessories">
-    //                             <div className="type-related-accessories">{accessory.type}</div>
-    //                             <div className="brand-related-accessories">{accessory.brand}</div>
-    //                             <div className="name-related-accessories">{accessory.name}</div>
-    //                         </div>
-    //                     </Link>
-    //                 </div>
-    //             ))}
-    //         </div>
-    //         {accessories.length > cardCount && (
-    //             <div className="slider-controls-related-accessories">
-    //                 <button className="prev-button" onClick={handlePrevClick} disabled={currentIndex === 0}>
-    //                     &#8592; Назад
-    //                 </button>
-    //                 <button className="next-button" onClick={handleNextClick} disabled={currentIndex + cardCount >= accessories.length}>
-    //                     Вперёд &#8594;
-    //                 </button>
-    //             </div>
-    //         )}
-    //     </div>
-    // );
 
     return (
         <div className="related-accessories">
