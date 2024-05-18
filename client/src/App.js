ab83aa848cd6a97640a427e9c335974ebdf11273
bakytdeveloper <bakytdeveloper@gmail.com>
2024-05-18 21:37:18 +0600

    Переделал шапку сайта

--- client/src/App.js
+++ client/src/App.js
@@ -100,7 +100,7 @@ const App = () => {
                 <Switch>
 
 
-                    <Route path="/home" exact>
+                    <Route path="/" exact>
                         <Home />
                     </Route>
 
