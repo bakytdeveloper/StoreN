819df14fa1e621c1e5b5d70c7b6a787c96fee123
bakytdeveloper <bakytdeveloper@gmail.com>
2024-05-27 01:47:31 +0600

    Создал модальное окно для контактов

---
+++ client/src/components/Footer/ContactInfoModal.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import tik from './../Header/tik-tok.png';
+import what from './../Header/whatsapp1.png';
+import ins from './../Header/instagram1.png';
+import tel from './../Header/telegram1.png';
+import {useHistory} from "react-router-dom";
+
+const ContactInfoModal = ({handleCloseModal}) => {
+    const history = useHistory();
+
+    const handleGoBack = () => {
+        history.push('/catalog'); // Переход на предыдущую страницу
+    };
+
+
+
+
+    return (
+        <div className="contact-info-modal">
+            <button className="close-btn-modal" onClick={handleCloseModal}>&times;</button> {/* Кнопка "крестик" */}
+
+            <h2 className="contact-info-modal-title">Наши контакты</h2>
+            {/*<div className="close-btn-modal" onClick={handleCloseModal}>Закрыть</div>*/}
+            {/*<button onClick={handleCloseModal}>&times;</button> /!* Кнопка "крестик" *!/*/}
+
+            <div className="phone-modal">
+                <a className="phone-link-modal" href="tel:+996508100777"  target="_blank">0(508) 100 777</a>
+            </div>
+            <div className="social-icons-modal">
+                <a href="https://www.tiktok.com/" target="_blank">
+                    <img className="icon-modal" src={tik} alt="TikTok Icon" />
+                </a>
+                <a href="https://api.whatsapp.com/send?phone=996508100777" target="_blank">
+                    <img className="icon-modal" src={what} alt="WhatsApp Icon" />
+                </a>
+                <a href="https://www.instagram.com/"  target="_blank" >
+                    <img className="icon-modal ins-modal" src={ins} alt="Instagram Icon" />
+                </a>
+                <a href="https://t.me/kanatasa?phone=+996508100777" target="_blank">
+                    <img className="icon-modal" src={tel} alt="Telegram Icon" />
+                </a>
+            </div>
+            <div className="workingTime-modal">с ПН по ВС - с 10:00 до 21:00</div>
+        </div>
+    );
+};
+
+export default ContactInfoModal;
--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -7,6 +7,8 @@ import catalogPageIcon from './bucklet-icon.png';
 import cartIcon from './cart-icon.png';
 import profileIcon from './priffile-icon.png';
 import contactIcon from './contact.png'
+// import ContactInfo from "../Header/ContactInfo";
+import ContactInfoModal from "./ContactInfoModal";
 
 // const Footer = ({ onSearch, cartItems = [], showSidebar, setShowSidebar, selectedOption, setSelectedOption, resetFilter, setCurrentPage }) => {
 //     const [searchTerm, setSearchTerm] = useState('');
@@ -207,9 +209,158 @@ import contactIcon from './contact.png'
 
 
 
+// const Footer = ({ onSearch, cartItems = [], showSidebar, setShowSidebar, selectedOption, setSelectedOption, resetFilter, setCurrentPage }) => {
+//     const [searchTerm, setSearchTerm] = useState('');
+//     const [isProfileOpen, setIsProfileOpen] = useState(false);
+//     const isAuthenticated = localStorage.getItem('token');
+//     const history = useHistory();
+//     const profileRef = useRef(null);
+//     const [showSellerRegistration, setShowSellerRegistration] = useState(false);
+//     const location = useLocation();
+//
+//     useEffect(() => {
+//         function handleClickOutside(event) {
+//             if (profileRef.current && !profileRef.current.contains(event.target)) {
+//                 setIsProfileOpen(false);
+//             }
+//         }
+//         document.addEventListener("mousedown", handleClickOutside);
+//         return () => {
+//             document.removeEventListener("mousedown", handleClickOutside);
+//         };
+//     }, []);
+//
+//     const handleSearchChange = (e) => {
+//         const value = e.target.value;
+//         setSearchTerm(value);
+//         setCurrentPage(1);
+//         onSearch(value);
+//     };
+//
+//     const handleCartClick = () => {
+//         if (cartItems.length > 0) {
+//             history.push("/cart");
+//         } else {
+//             history.push("/");
+//         }
+//     };
+//
+//
+//
+//     const handleContactClick = () => {
+//         setSelectedOption('contact');
+//         setShowSidebar(!showSidebar);
+//     };
+//
+//     const handleCatalogClick = () => {
+//         setSelectedOption('catalog');
+//         setShowSidebar(!showSidebar);
+//     };
+//
+//     const handleProfileClick = () => {
+//         setIsProfileOpen(!isProfileOpen);
+//         const role = localStorage.getItem('role');
+//         if (isAuthenticated) {
+//             if (role === 'customer') {
+//                 history.push("/profile");
+//             } else if (role === 'seller') {
+//                 history.push("/sellerProfile");
+//             }
+//         }
+//     };
+//
+//     const handleLoginClick = () => {
+//         if (isAuthenticated) {
+//             history.push("/profile");
+//         } else {
+//             history.push("/login");
+//         }
+//         setIsProfileOpen(false);
+//     };
+//
+//     const handleLogoutClick = () => {
+//         localStorage.removeItem('token');
+//         history.push("/");
+//         setIsProfileOpen(false);
+//     };
+//
+//     const handlePartnerClick = () => {
+//         setIsProfileOpen(false);
+//         history.push("/sellers/register");
+//     };
+//
+//     const handleCloseSidebar = () => {
+//         setShowSidebar(false);
+//     };
+//
+//     const totalItemsCount = cartItems.length > 0 ? cartItems.reduce((total, item) => total + item.quantity, 0) : 0;
+//
+//     return (
+//         <div className="footer-page">
+//             <div className="home-icon">
+//                 <img className="home-icon-img" src={homeIcon} />
+//                 <div>
+//                     <Link className="home-icon-link" to="/" >Главная</Link>
+//                 </div>
+//             </div>
+//
+//             <div className="catalog-icon">
+//                 <img className="catalog-icon-img" src={catalogPageIcon} />
+//                 <div>
+//                     <Link className="catalog-icon-link catalog-header" to="/catalog" >Каталог</Link>
+//                 </div>
+//             </div>
+//
+//             <div className="profile-icon">
+//                 <img className="profile-icon-img" src={profileIcon} />
+//                 <div className="footer-profileIcon" ref={profileRef}>
+//                     {!isProfileOpen && (
+//                         <span className="profile-icon-link" onClick={handleProfileClick}>Войти</span>
+//                     )}
+//                     {isProfileOpen && (
+//                         <div className="footer-dropdown-menu">
+//                             <button onClick={handleLoginClick}>{isAuthenticated ? "Профиль" : "Логин"}</button>
+//                             {!isAuthenticated && <button className="footer-dropdown-menu-partner" onClick={handlePartnerClick}>Партнёр</button>}
+//                             {isAuthenticated && <button className="footer-dropdown-menu-logout" onClick={handleLogoutClick}>Выход</button>}
+//                         </div>
+//                     )}
+//                 </div>
+//                 {showSellerRegistration && (
+//                     <SellerRegistrationForm />
+//                 )}
+//             </div>
+//
+//             <div className="cart-icon">
+//                 <img className="cart-icon-img" src={cartIcon}/>
+//                 <span>({totalItemsCount})</span>
+//                 <div>
+//                     <Link to="/cart" className="footer-auth-button btn" onClick={handleCartClick}>
+//                         <span className="footer-totalItems">Корзина</span>
+//                     </Link>
+//                 </div>
+//             </div>
+//
+//             <div className="contact-icon">
+//                 <img className="contact-icon-img" src={contactIcon}/>
+//                 <div>
+//                     <Link to="/contact" className="footer-btn">Контакты</Link>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+//
+// export default Footer;
+
+
+
+
+
+
 const Footer = ({ onSearch, cartItems = [], showSidebar, setShowSidebar, selectedOption, setSelectedOption, resetFilter, setCurrentPage }) => {
     const [searchTerm, setSearchTerm] = useState('');
     const [isProfileOpen, setIsProfileOpen] = useState(false);
+    const [isContactModalOpen, setIsContactModalOpen] = useState(false); // Состояние для управления модальным окном контактов
     const isAuthenticated = localStorage.getItem('token');
     const history = useHistory();
     const profileRef = useRef(null);
@@ -243,11 +394,12 @@ const Footer = ({ onSearch, cartItems = [], showSidebar, setShowSidebar, selecte
         }
     };
 
-    
-
     const handleContactClick = () => {
-        setSelectedOption('contact');
-        setShowSidebar(!showSidebar);
+        setIsContactModalOpen(true); // Открыть модальное окно
+    };
+
+    const handleCloseModal = () => {
+        setIsContactModalOpen(false); // Закрыть модальное окно
     };
 
     const handleCatalogClick = () => {
@@ -341,9 +493,18 @@ const Footer = ({ onSearch, cartItems = [], showSidebar, setShowSidebar, selecte
             <div className="contact-icon">
                 <img className="contact-icon-img" src={contactIcon}/>
                 <div>
-                    <Link to="/contact" className="footer-btn">Контакты</Link>
+                    <span className="footer-btn" onClick={handleContactClick}>Контакты</span>
                 </div>
             </div>
+
+            {isContactModalOpen && (
+                <div className="modal">
+                    <div className="modal-content">
+                        <ContactInfoModal handleCloseModal={handleCloseModal} />
+                        {/*<button className="close-btn" onClick={handleCloseModal}>Закрыть</button>*/}
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
