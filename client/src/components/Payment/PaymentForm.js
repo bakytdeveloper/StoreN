38f91a48b37c6f3857c847eead77150d07dbe325
bakytdeveloper <bakytdeveloper@gmail.com>
2024-06-13 22:07:43 +0600

    Поправил слово позвонить на позвонит

--- client/src/components/Payment/PaymentForm.js
+++ client/src/components/Payment/PaymentForm.js
@@ -4,7 +4,7 @@ const PaymentForm = () => {
     return (
         <div className="payment-form" style={{textAlign:"center"}}>
             <h1 className="payment-form-h1">Спасибо, ваш заказ принят</h1>
-            <h2 className="payment-form-h2-1">Вам перезвонить оператор через несколько минут</h2>
+            <h2 className="payment-form-h2-1">Вам перезвонит оператор через несколько минут</h2>
             <h2 className="payment-form-h2-2" style={{color:"blue"}}>Ожидайте звонка</h2>
         </div>
     );
