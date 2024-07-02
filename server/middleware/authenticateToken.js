d50b1a21717f537c84e5e29548d9b888239c0cba
bakytdeveloper <bakytdeveloper@gmail.com>
2024-07-02 17:48:59 +0600

    Восстанавливаю гит, добавил логи для прослушки мидлвеаров

--- server/middleware/authenticateToken.js
+++ server/middleware/authenticateToken.js
@@ -1,3 +1,35 @@
+// const jwt = require('jsonwebtoken');
+//
+// const authenticateToken = (req, res, next) => {
+//     const token = req.headers.authorization && req.headers.authorization.split(' ')[1];
+//     console.log('Token:', token);
+//     if (!token) {
+//         return res.status(401).json({ message: 'Unauthorized' });
+//     }
+//     jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+//         if (err) {
+//             console.log('Invalid token:', err);
+//             return res.status(401).json({ message: 'Invalid token' });
+//         }
+//         req.user = user; // Устанавливаем данные пользователя в req.user
+//         next();
+//     });
+// };
+//
+// const checkRole =(roles) => {
+//     return (req, res, next) => {
+//         if (!req.user || !roles.includes(req.user.role)) {
+//             return res.status(403).json({ message: 'Access denied' });
+//         }
+//         next();
+//     };
+// };
+//
+//
+//
+// module.exports = {authenticateToken, checkRole};
+
+
 const jwt = require('jsonwebtoken');
 
 const authenticateToken = (req, res, next) => {
@@ -16,15 +48,15 @@ const authenticateToken = (req, res, next) => {
     });
 };
 
-const checkRole =(roles) => {
+const checkRole = (roles) => {
     return (req, res, next) => {
+        console.log('User role:', req.user.role);
         if (!req.user || !roles.includes(req.user.role)) {
             return res.status(403).json({ message: 'Access denied' });
         }
         next();
     };
 };
-
 
 
 module.exports = {authenticateToken, checkRole};
