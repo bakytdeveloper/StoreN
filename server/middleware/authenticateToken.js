e6966787055d0f0e35327d93bcd1862f7e7f3cde
bakytdeveloper <bakytdeveloper@gmail.com>
2024-07-03 00:30:43 +0600

    Обновил код по отображению заказов

--- server/middleware/authenticateToken.js
+++ server/middleware/authenticateToken.js
@@ -32,10 +32,39 @@
 
 const jwt = require('jsonwebtoken');
 
+// const authenticateToken = (req, res, next) => {
+//     const token = req.headers.authorization && req.headers.authorization.split(' ')[1];
+//     console.log('Token:', token);
+//     if (!token) {
+//         return res.status(401).json({ message: 'Unauthorized' });
+//     }
+//     jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+//         if (err) {
+//             console.log('Invalid token:', err);
+//             return res.status(401).json({ message: 'Invalid token' });
+//         }
+//         req.user = user; // Устанавливаем данные пользователя в req.user
+//         next();
+//     });
+// };
+//
+// const checkRole = (roles) => {
+//     return (req, res, next) => {
+//         console.log('User role:', req.user.role);
+//         if (!req.user || !roles.includes(req.user.role)) {
+//             return res.status(403).json({ message: 'Access denied' });
+//         }
+//         next();
+//     };
+// };
+
+
+
 const authenticateToken = (req, res, next) => {
     const token = req.headers.authorization && req.headers.authorization.split(' ')[1];
     console.log('Token:', token);
     if (!token) {
+        console.log('No token found');
         return res.status(401).json({ message: 'Unauthorized' });
     }
     jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
@@ -44,6 +73,7 @@ const authenticateToken = (req, res, next) => {
             return res.status(401).json({ message: 'Invalid token' });
         }
         req.user = user; // Устанавливаем данные пользователя в req.user
+        console.log('Authenticated user:', user);
         next();
     });
 };
@@ -52,11 +82,13 @@ const checkRole = (roles) => {
     return (req, res, next) => {
         console.log('User role:', req.user.role);
         if (!req.user || !roles.includes(req.user.role)) {
+            console.log('Access denied for user role:', req.user.role);
             return res.status(403).json({ message: 'Access denied' });
         }
         next();
     };
 };
+
 
 
 module.exports = {authenticateToken, checkRole};
