7b288ae04232cd7d4928a9dbe69a9f38ec1a9e3a
bakytdeveloper <bakytdeveloper@gmail.com>
2024-07-04 15:41:25 +0600

    Поправил отображение общей цены товара в Деталях заказа

--- client/src/components/Cart/CartSummary.js
+++ client/src/components/Cart/CartSummary.js
@@ -8,7 +8,7 @@ import "./CartSummary.css"
 const CartSummary = ({ totalPrice, totalItems }) => {
     return (
         <div className="checkList">
-            <div className="total-price">Общая сумма: {totalPrice} сом</div>
+            <div className="total-price">Общая сумма: <strong>{totalPrice}</strong> сом</div>
             {/*<div className="total-items">Общее количество товара: {totalItems}</div>*/}
         </div>
     );
