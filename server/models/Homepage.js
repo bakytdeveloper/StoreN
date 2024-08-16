03a8e60a41210169b4dd7c75d07af39b90225a92
bakytdeveloper <bakytdeveloper@gmail.com>
2024-08-16 21:08:04 +0600

    Обновил код на сервере, для обработки информации на главной странице

--- server/models/Homepage.js
+++ server/models/Homepage.js
@@ -1,20 +1,45 @@
+// // models/Homepage.js
+// const mongoose = require('mongoose');
+//
+// const homepageSchema = new mongoose.Schema({
+//     sliderImages: [String], // Массив URL-ов картинок для слайдера
+//     genderImages: [String], // Массив URL-ов картинок для полов
+//     promotions: {
+//         title: String, // Название акции
+//         description: String, // Описание акции
+//         startDate: Date, // Дата начала акции
+//         endDate: Date // Дата окончания акции
+//     }
+// });
+//
+// // module.exports = mongoose.model('Homepage', homepageSchema);
+//
+//
+//
+// const Homepage = mongoose.model('Homepage', homepageSchema);
+//
+// module.exports = Homepage;
+
+
 // models/Homepage.js
 const mongoose = require('mongoose');
 
-const homepageSchema = new mongoose.Schema({
-    sliderImages: [String], // Массив URL-ов картинок для слайдера
-    genderImages: [String], // Массив URL-ов картинок для полов
-    promotions: {
-        title: String, // Название акции
-        description: String, // Описание акции
-        startDate: Date, // Дата начала акции
-        endDate: Date // Дата окончания акции
-    }
+const promotionSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    startDate: Date,
+    endDate: Date
 });
 
-// module.exports = mongoose.model('Homepage', homepageSchema);
+const sliderImageSchema = new mongoose.Schema({
+    url: String,
+    promotions: [promotionSchema]
+});
 
-
+const homepageSchema = new mongoose.Schema({
+    sliderImages: [sliderImageSchema],
+    genderImages: [String]
+});
 
 const Homepage = mongoose.model('Homepage', homepageSchema);
 
