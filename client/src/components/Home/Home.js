812d77460a61de426ccd32c61a3a69d1c4d33870
bakytdeveloper <bakytdeveloper@gmail.com>
2024-08-19 23:02:31 +0600

    Обновил адаптивный вариант, отображения главной страницы

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -158,7 +158,7 @@ const Home = ({ setShowSidebar, setIsFooterCatalog, setSelectedGender, setSearch
                             style={{ height: '100%', border:"none" }} // Высота карусели
                         >
                             <img src={slide.url} className="d-block w-100 carousel-item-img" alt={`Slide ${index + 1}`} />
-                            <div className="carousel-caption d-none d-md-block carousel-caption-title-description">
+                            <div className="carousel-caption d-md-block carousel-caption-title-description">
                                 <div className="carousel-caption-title" style={{ color: slide.colorTitle || '#000000', fontSize: slide.fontSizeTitle, fontFamily: slide.fontFamilleTitle || 'Arial' }}>{slide.promotions[0]?.title || 'ГОТОВЬСЯ К ЛЕТУ'}</div>
                                 <div className="carousel-caption-description" style={{ color: slide.colorDescription || '#000000', fontSize: slide.fontSizeDescription, fontFamily: slide.fontFamilleDescription || 'Arial' }}>{slide.promotions[0]?.description || 'НОВАЯ КОЛЛЕКЦИЯ ВОШЛА В ЧАТ'}</div>
                             </div>
