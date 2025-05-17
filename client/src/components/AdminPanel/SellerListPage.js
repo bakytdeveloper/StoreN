38b76f669915df48f8b9ffba108f37a0442fce18
bakytdeveloper <bakytdeveloper@gmail.com>
2025-05-17 12:43:22 +0600

    Обновил визуалку страницы продавцов, в Админ панеле

--- client/src/components/AdminPanel/SellerListPage.js
+++ client/src/components/AdminPanel/SellerListPage.js
@@ -112,75 +112,77 @@ const SellerListPage = ({ setShowSidebar }) => {
 
     return (
         <div className="sellersListPage">
-            <h2 className="sellerTitle">Список продавцов</h2>
-            <div className="sellersListClose" type="button" onClick={handleClose}>
-                <span> &#10006;</span>
-            </div>
-            <table>
-                <thead>
-                <tr>
-                    <th>№</th>
-                    <th>Название компании</th>
-                    <th>Имя Фам.</th>
-                    <th>Email</th>
-                    <th>Телефон</th>
-                    <th style={{ padding: '0 25px', width:"177px" }}>Направление компания</th>
-                    <th>Время</th>
-                    <th>Статус</th>
-                    <th>Время изменения статуса</th>
-                    <th>Действия</th>
-                    <th>Товары продавца</th>
-                </tr>
-                </thead>
-                <tbody>
-                {sellers.slice().reverse().map((seller, index) => (
-                    <tr key={seller._id}>
-                        <td style={{fontWeight:"bold"}}>{index + 1}</td>
-                        <td>{seller.companyName}</td>
-                        <td>{seller.name}</td>
-                        <td>{seller.email}</td>
-                        <td>{seller.phoneNumber}</td>
-                        <td>
+          <div className="sellers-list-page-table">
+              <h2 className="sellerTitle">Список продавцов</h2>
+              <div className="sellersListClose" type="button" onClick={handleClose}>
+                  <span> &#10006;</span>
+              </div>
+              <table>
+                  <thead>
+                  <tr>
+                      <th>№</th>
+                      <th>Название компании</th>
+                      <th>Имя Фам.</th>
+                      <th>Email</th>
+                      <th>Телефон</th>
+                      <th style={{ padding: '0 25px', width:"177px" }}>Направление компания</th>
+                      <th>Время</th>
+                      <th>Статус</th>
+                      <th>Время изменения статуса</th>
+                      <th>Действия</th>
+                      <th>Товары продавца</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  {sellers.slice().reverse().map((seller, index) => (
+                      <tr key={seller._id}>
+                          <td style={{fontWeight:"bold"}}>{index + 1}</td>
+                          <td>{seller.companyName}</td>
+                          <td>{seller.name}</td>
+                          <td>{seller.email}</td>
+                          <td>{seller.phoneNumber}</td>
+                          <td>
                             <textarea className="seller-list-page-textarea"
                                       value={seller.companyDescription}
                                       readOnly
                             />
-                        </td>
-                        <td>{new Date(seller.createdAt).toLocaleString()}</td>
-                        <SellerItem key={seller._id} seller={seller} onUpdateStatus={updateStatusSeller} />
-                        <td>
-                            {seller.statusHistory && seller.statusHistory.length > 0
-                                ? new Date(seller.statusHistory[seller.statusHistory.length - 1].time).toLocaleString()
-                                : '-'}
-                        </td>
-                        <td>
-                            <button className="delete-button" onClick={() => handleDeleteClick(seller)}>Удалить</button>
-                        </td>
-                        <td style={{ background: seller.isProductsVisible ? '#08a911' : '#ee579e', textAlign: "center", color: 'white', padding: '5px' }}>
-                            {seller.isProductsVisible ? (
-                                <>
-                                    <span role="img" aria-label="checkmark" style={{ fontSize: '20px' }}>✔️</span>
-                                    {` ${new Date(seller.lastVisibilityChange).toLocaleString()}`}
-                                </>
-                            ) : (
-                                <>
-                                    <span role="img" aria-label="exclamation" style={{ fontSize: '20px' }}>❗</span>
-                                    {` ${new Date(seller.lastVisibilityChange).toLocaleString()}`}
-                                </>
-                            )}
-                        </td>
-                    </tr>
-                ))}
-                </tbody>
-            </table>
-            {selectedSeller && (
-                <ConfirmationModal
-                    show={showModal}
-                    onClose={handleModalClose}
-                    onConfirm={handleDeleteSeller}
-                    seller={selectedSeller}
-                />
-            )}
+                          </td>
+                          <td>{new Date(seller.createdAt).toLocaleString()}</td>
+                          <SellerItem key={seller._id} seller={seller} onUpdateStatus={updateStatusSeller} />
+                          <td>
+                              {seller.statusHistory && seller.statusHistory.length > 0
+                                  ? new Date(seller.statusHistory[seller.statusHistory.length - 1].time).toLocaleString()
+                                  : '-'}
+                          </td>
+                          <td>
+                              <button className="delete-button" onClick={() => handleDeleteClick(seller)}>Удалить</button>
+                          </td>
+                          <td  className="checkmark-button"  style={{ background: seller.isProductsVisible ? '#08a911' : '#ee579e'}}>
+                              {seller.isProductsVisible ? (
+                                  <>
+                                      <span role="img" aria-label="checkmark" style={{ fontSize: '20px' }}>✔️</span>
+                                      {` ${new Date(seller.lastVisibilityChange).toLocaleString()}`}
+                                  </>
+                              ) : (
+                                  <>
+                                      <span role="img" aria-label="exclamation" style={{ fontSize: '20px' }}>❗</span>
+                                      {` ${new Date(seller.lastVisibilityChange).toLocaleString()}`}
+                                  </>
+                              )}
+                          </td>
+                      </tr>
+                  ))}
+                  </tbody>
+              </table>
+              {selectedSeller && (
+                  <ConfirmationModal
+                      show={showModal}
+                      onClose={handleModalClose}
+                      onConfirm={handleDeleteSeller}
+                      seller={selectedSeller}
+                  />
+              )}
+          </div>
         </div>
     );
 };
